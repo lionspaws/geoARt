70c29c6a210a3622833cecf44863f77108504615
Helen Miles <hem23@aber.ac.uk>
2021-10-22 10:15:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function staticLoadPlaces() {
         {
             name: 'PSB Fish Pond',
             files: './assets/simple-spotted-jellyfish-baked-animation/Spotted-Jelly.gltf',
+            scale: '0.3 0.3 0.3',
             location: {
                 lat: 52.415741,
                 lng: -4.065626,
@@ -16,6 +17,7 @@ function staticLoadPlaces() {
         {
             name: 'Wood nr Robotics Workshop',
             files: './assets/old_chest/scene.gltf',
+            scale: '0.5 0.5 0.5',
             location: {
                 lat: 52.415450,
                 lng: -4.065079,
@@ -24,6 +26,7 @@ function staticLoadPlaces() {
         {
             name: 'Arts Centre Plaza',
             files: './assets/azeria/scene.gltf',
+            scale: '1 1 1',
             location: {
                 lat: 52.415666,
                 lng: -4.063480,
@@ -39,13 +42,14 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
         let modelfiles = place.files;
+        let modelscale = place.scale;
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', modelfiles);
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', modelscale);
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
